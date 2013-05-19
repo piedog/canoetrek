400709ef3dcc752395c40a33e7c5bb9b71d681c7
Rob <rob@piepul.com>
2013-05-18 21:07:58 -0500

    micropost ordering and destruction

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,10 +15,10 @@ FactoryGirl.define do
         factory :admin do
             admin true
         end
+    end
 
-   #factory :micropost do
-   #    content "Lorem ipsum"
-   #    user
-   #end
+    factory :micropost do
+        content "Lorem ipsum"
+        user
     end
 end
