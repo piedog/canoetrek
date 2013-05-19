c9b89b450c181b341c6bb956d3c61fd18ef91740
Rob <rob@piepul.com>
2013-05-18 21:16:56 -0500

    added micropost validation for presence and length

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Micropost do
 
     let(:user) { FactoryGirl.create(:user) }
+    before { @micropost = user.microposts.build(content: "Lorem ipsum") }
  #  before do
  #      ### THis code is wrong
  #      @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
  #  end
-    before { @micropost = user.microposts.build(content: "Lorem ipsum") }
 
     subject { @micropost }
 
@@ -28,4 +28,16 @@ describe Micropost do
             end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
         end
     end
+
+
+    describe "with blank content" do
+        before { @micropost.content = ' ' }
+        it { should_not be_valid }
+    end
+
+    describe "with content that is too long" do
+        before { @micropost.content = "a" * 141 }
+        it { should_not be_valid }
+    end
+
 end
