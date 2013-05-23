976f10d1be73e22039fbf30bcf3b7c1de69a3c43
Rob <rob@piepul.com>
2013-05-22 19:21:43 -0500

    create microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,15 @@
 class StaticPagesController < ApplicationController
-  def map
-    gon.mapoptions = {
-        msg: 'hello world',
-        centerx: -10545985,
-        centery:   4323684,
-        zoom:      9,
-        mapdiv:  'map-container'
-    }
-  end
+    def map
+        gon.mapoptions = {
+            msg: 'hello world',
+            centerx: -10545985,
+            centery:   4323684,
+            zoom:      9,
+            mapdiv:  'map-container'
+        }
+    end
 
-  def home
-  end
+    def home
+        @micropost = current_user.microposts.build if signed_in? 
+    end
 end
