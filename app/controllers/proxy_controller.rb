cab7e0b9c20f9d83da05e9ed9e8d284c54f47be4
Rob <rob@piepul.com>
2013-07-15 18:44:08 -0500

    override bootstrap styles; add class names to h1 to bring the h1 title down; change gravatar url

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 class ProxyController < ApplicationController
     def get
+        logger.debug "In proxy";
         url = URI.parse(params["url"])
         result = Net::HTTP.get_response(url)
         send_data :text => result.body
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,10 @@ module UsersHelper
 
     def gravatar_for(user, options = { size: 50 })
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "https//secure.gravatar.com/avatar/#{gravatar_id}"
+        size = options[:size]
+        gravatar =
+        ##gravatar_url = "https//secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
 end
