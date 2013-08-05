b358b9834fe2137ec05ffa7eede8b26f45c58d4d
Rob <rob@piepul.com>
2013-08-04 21:33:49 -0500

    deleteed TripArea model

--- app/models/trip_area.rb
+++
@@ -1,8 +0,0 @@
-class TripArea < ActiveRecord::Base
-    belongs_to :user
-    attr_accessible :center_point, :description, :name, :user_id, :zoom_level
-    default_scope -> { order('created_at DESC') }
-    validates :user_id, presence: true
-    validates :name, presence: true, length: { maximum: 16 }
-    set_rgeo_factory_for_column(:center_point, RGeo::Geographic.spherical_factory(:srid => 4326))
-end
