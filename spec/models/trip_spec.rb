8e3d525b0a6a132b77917525764aa12628137e52
Rob <rob@piepul.com>
2013-08-06 21:39:43 -0500

    added spec for Trip model

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Trip do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+        #not really right
+        @trip = Trip.new(name: "trip01",description: "This is trip number one", center: "POINT(-91.05 39.44)",zoom: 7, user_id: user.id)
+    end
+
+
+    subject { @trip }
+    it { should respond_to (:name) }
+    it { should respond_to (:description) }
+    it { should respond_to (:center) }
+    it { should respond_to (:zoom) }
+    it { should respond_to (:user_id) }
 end
