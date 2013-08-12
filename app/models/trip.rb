947cc0c377eb1205f368ad995d814873b3d06b3b
Rob <rob@piepul.com>
2013-08-11 20:12:37 -0500

    validate code for center point changes; do not know if it works

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ActiveRecord::Base
     attr_accessible :center, :description, :name, :zoom, :user_id
-    set_rgeo_factory_for_column(:center, RGeo::Geographic.spherical_factory(:srid => 4326))
+    set_rgeo_factory_for_column(:center, RGeo::Geographic.spherical_factory(srid: 4326))
 
     belongs_to :user
     default_scope -> { order('created_at DESC') }
@@ -9,4 +9,13 @@ class Trip < ActiveRecord::Base
     validates :name, presence: true, length: { maximum: 32 }
 
     validates_numericality_of :zoom, allow_nil: false, in: 1..12, message: "a zoom level must be provided between 1 and 12"
+
+
+
+
+
+    delegate :latitude, to: :center, allow_nil: true
+    delegate :longitude, to: :center, allow_nil: true
+    validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+    validates :longitude,presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
